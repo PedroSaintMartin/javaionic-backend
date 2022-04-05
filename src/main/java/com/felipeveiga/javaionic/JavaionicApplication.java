d2cccb258a5fc025bf5f1ab94b12007b1cc67391
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2022-04-05 11:28:38 -0300

    Criação do Projeto

---
+++ src/main/java/com/felipeveiga/javaionic/JavaionicApplication.java
@@ -0,0 +1,13 @@
+package com.felipeveiga.javaionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaionicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaionicApplication.class, args);
+	}
+
+}
